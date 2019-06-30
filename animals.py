b0c5ba2123be6c4de88e6f66243a02aadcbc9d99
James Bradbury <JamesBradbury@users.noreply.github.com>
2019-06-30 21:28:24 +0100

    Initial commit
    This is for illustration only. I don't recommend using this code!

---
+++ animals.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+def most_common_animal(observed_items):
+    known_animal_names = {"dog", "cat", "bear", "gruffalo"}
+    occurrences = defaultdict(int)
+    for item in observed_items:
+        if item in known_animal_names:
+            print(item, "is known.")
+        else:
+            print(item, "is unknown.")
+        occurrences[item] += 1
+    return max(occurrences, key=occurrences.get)
