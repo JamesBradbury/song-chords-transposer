7b30153655f37243e0827a775199f1e1da2bf0b1
James Bradbury <JamesBradbury@users.noreply.github.com>
2019-06-30 21:29:03 +0100

    Remove gruffalo

--- animals.py
+++ animals.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 def most_common_animal(observed_items):
-    known_animal_names = {"dog", "cat", "bear", "gruffalo"}
+    known_animal_names = {"dog", "cat", "bear"}
     occurrences = defaultdict(int)
     for item in observed_items:
         if item in known_animal_names:
